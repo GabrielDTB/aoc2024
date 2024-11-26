0c0143f1ceff883bcfb411ab71363ee030575edd
Gabriel Talbert Bunt <gabriel@gabrieltb.me>
2024-11-25 20:11:16 -0500

    Add flake dev shell

---
+++ flake.nix
@@ -0,0 +1,38 @@
+{
+  description = "Rust dev shell";
+
+  inputs = {
+    nixpkgs.url      = "github:NixOS/nixpkgs/nixos-unstable";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+    flake-utils.url  = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, rust-overlay, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs {
+          inherit system overlays;
+        };
+      in
+      {
+        devShells.default = with pkgs; mkShell.override{
+          stdenv = stdenvAdapters.useMoldLinker clangStdenv;
+        } {
+          buildInputs = [
+            openssl
+            pkg-config
+            aoc-cli
+            (rust-bin.selectLatestNightlyWith (toolchain: toolchain.default.override {
+              extensions = [
+                "rust-src"
+                "rust-analyzer"
+                "rustfmt"
+              ];
+            }))
+          ];
+        };
+      }
+    );
+}
+
