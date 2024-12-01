d976b3a03deee57cae578cad2c7569973898844a
Gabriel Talbert Bunt <gabriel@gabrieltb.me>
2024-12-01 16:20:02 -0500

    Complete day 1 part 1

--- src/bin/01.rs
+++ src/bin/01.rs
@@ -1,9 +1,26 @@
 advent_of_code::solution!(1);
-use anyhow::{bail, Result};
+use anyhow::{bail, Context, Result};
 use itertools::Itertools;
 
+fn parse_input(input: &str) -> Result<(Vec<u32>, Vec<u32>)> {
+    let result = input
+        .lines()
+        .flat_map(|line| line.split_whitespace())
+        .map(|item| item.parse::<u32>())
+        .process_results(|iter| iter.tuples().unzip())?;
+    Ok(result)
+}
+
 pub fn part_one(input: &str) -> Result<u32> {
-    bail!("Solution not implemented")
+    let (mut left, mut right) = parse_input(input)?;
+    left.sort();
+    right.sort();
+    let answer = left
+        .into_iter()
+        .zip(right)
+        .map(|(l, r)| if l >= r { l - r } else { r - l })
+        .sum();
+    Ok(answer)
 }
 
 pub fn part_two(input: &str) -> Result<u32> {
