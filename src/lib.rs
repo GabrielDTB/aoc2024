9b8c3309a5dddfff0833e25ae9365e6a487db318
Gabriel Talbert Bunt <gabriel@gabrieltb.me>
2024-12-01 17:39:59 -0500

    Add helper function count_occurrences
    Counts the occurrences of each element in an iterable and returns
    a map of element -> count.

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,20 @@
+use std::collections::HashMap;
+use std::hash::Hash;
+
 pub mod template;
 
-// Use this file to add helper functions and additional modules.
+// We're never going to have arrays with > u32::MAX elements,
+// let alone the same element, in AOC.
+pub fn count_occurrences<T>(iter: impl IntoIterator<Item = T>) -> HashMap<T, u32>
+where
+    T: Eq + Hash,
+{
+    let mut counts = HashMap::new();
+    for item in iter.into_iter() {
+        counts
+            .entry(item)
+            .and_modify(|count| *count += 1)
+            .or_insert(1);
+    }
+    counts
+}
