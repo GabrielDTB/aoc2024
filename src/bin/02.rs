a3213e726520a6ce84507eb82c2a8359f0395214
Gabriel Talbert Bunt <gabriel@gabrieltb.me>
2024-12-02 10:48:22 -0500

    Complete day 1 part 1

--- src/bin/02.rs
+++ src/bin/02.rs
@@ -1,9 +1,46 @@
 advent_of_code::solution!(2);
-use anyhow::{bail, Result};
+use anyhow::{bail, Context, Result};
 use itertools::Itertools;
 
+fn parse_input(input: &str) -> Result<Vec<Vec<u32>>> {
+    let result = input
+        .lines()
+        .map(|l| {
+            l.split_whitespace()
+                .map(|e| e.parse::<u32>().with_context(|| "Failed to parse '{e}'."))
+                .collect::<Result<Vec<_>>>()
+        })
+        .collect::<Result<Vec<Vec<_>>>>()?;
+    Ok(result)
+}
+
+fn good_report(input: Vec<u32>) -> bool {
+    if input.len() < 2 {
+        return false;
+    }
+    let condition = if input[0] > input[1] {
+        |l, r| l > r && l - r >= 1 && l - r <= 3
+    } else {
+        |l, r| r > l && r - l >= 1 && r - l <= 3
+    };
+    for window in input.windows(2) {
+        if !condition(window[0], window[1]) {
+            return false;
+        }
+    }
+    return true;
+}
+
 pub fn part_one(input: &str) -> Result<u32> {
-    bail!("Solution not implemented")
+    let reports = parse_input(input)?;
+
+    let mut answer = 0;
+    for report in reports {
+        if good_report(report) {
+            answer += 1;
+        }
+    }
+    Ok(answer)
 }
 
 pub fn part_two(input: &str) -> Result<u32> {
