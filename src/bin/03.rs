3bb376b4b5876395387c9859858241fe52f3aae9
Gabriel Talbert Bunt <gabriel@gabrieltb.me>
2024-12-03 18:26:16 -0500

    Complete day 1 part 1

--- src/bin/03.rs
+++ src/bin/03.rs
@@ -1,9 +1,43 @@
 advent_of_code::solution!(3);
-use anyhow::{bail, Result};
+use anyhow::{anyhow, bail, Context, Result};
 use itertools::Itertools;
+use nom::{
+    branch::alt,
+    bytes::complete::{tag, take_until, take_while, take_while_m_n},
+    character::{complete::char, is_digit},
+    combinator::map,
+    multi::many0,
+    sequence::{delimited, preceded, separated_pair},
+    IResult,
+};
 
 pub fn part_one(input: &str) -> Result<u32> {
-    bail!("Solution not implemented")
+    let thing: IResult<&str, Vec<Option<(&str, &str)>>> = many0(preceded(
+        take_until("mul"),
+        alt((
+            map(
+                delimited(
+                    tag("mul("),
+                    separated_pair(
+                        take_while_m_n(1, 3, |c: char| is_digit(c as u8)),
+                        char(','),
+                        take_while_m_n(1, 3, |c: char| is_digit(c as u8)),
+                    ),
+                    char(')'),
+                ),
+                Some,
+            ),
+            map(preceded(tag("mul"), take_while(|c| c != 'm')), |_| None),
+        )),
+    ))(input);
+    let answer = thing
+        .unwrap()
+        .1
+        .into_iter()
+        .flatten()
+        .map(|(a, b)| a.parse::<u32>().unwrap() * b.parse::<u32>().unwrap())
+        .sum();
+    Ok(answer)
 }
 
 pub fn part_two(input: &str) -> Result<u32> {
